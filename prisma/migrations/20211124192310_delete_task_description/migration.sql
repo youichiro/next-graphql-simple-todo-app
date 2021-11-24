868d1b76ee8bb7d0df7a3c9e9fed7f3a0af662e5
youichiro <cinnamon416@gmail.com>
2021-11-25 04:24:03 +0900

    delete task description

---
+++ prisma/migrations/20211124192310_delete_task_description/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `description` on the `tasks` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "tasks" DROP COLUMN "description";
