5dfc55fb0147f33926981b93f3934dc5f1462b5b
youichiro <cinnamon416@gmail.com>
2021-11-27 06:38:21 +0900

    NexusでQueryを定義する

--- graphql/types/Task.ts
+++ graphql/types/Task.ts
@@ -1,4 +1,4 @@
-import { objectType } from 'nexus'
+import { objectType, extendType } from 'nexus'
 
 export const Task = objectType({
   name: 'Task',
@@ -6,5 +6,17 @@ export const Task = objectType({
     t.nonNull.int('id')
     t.nonNull.string('title')
     t.nonNull.boolean('done')
+  }
+})
+
+export const TasksQuery = extendType({
+  type: 'Query',
+  definition(t) {
+    t.nonNull.list.field('tasks', {
+      type: 'Task',
+      resolve(_parent, _args, ctx) {
+        return ctx.prisma.task.findMany()
+      }
+    })
   }
 })
