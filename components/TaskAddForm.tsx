af9065856bde8803b37b9459992cc121510ac705
youichiro <cinnamon416@gmail.com>
2021-11-27 10:16:12 +0900

    Formik

--- components/TaskAddForm.tsx
+++ components/TaskAddForm.tsx
@@ -1,18 +1,35 @@
+import { Formik, Field, Form } from 'formik';
 import { Stack, FormControl, Input, Button } from '@chakra-ui/react'
 
 
-  const TaskAddForm: React.FC = () => {
-    return (
-      <Stack direction='row'>
-        <FormControl>
-          <Input id='taskName' type='text' placeholder='Add task' />
-        </FormControl>
-        <Button colorScheme='teal' type='submit'>
-          Submit
-        </Button>
-      </Stack>
-
-    )
+const TaskAddForm: React.FC = () => {
+  const handleSubmit = (taskName: string, resetForm: () => void) => {
+    if (!taskName) return
+    alert(taskName)
+    resetForm()
   }
+
+  return (
+    <Formik
+      initialValues={{ taskName: '' }}
+      onSubmit={(value, actions) => handleSubmit(value.taskName, actions.resetForm)}
+    >
+      <Form>
+        <Stack direction='row'>
+          <Field name='taskName'>
+            {({ field }) => (
+              <FormControl>
+                <Input {...field} id='taskName' type='text' placeholder='Add task' />
+              </FormControl>
+            )}
+          </Field>
+          <Button colorScheme='teal' type='submit'>
+            Submit
+          </Button>
+        </Stack>
+      </Form>
+    </Formik>
+  )
+}
 
 export default TaskAddForm
