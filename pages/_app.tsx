ee956b88e4b53f2ceab385dfd1312c726f8f9b16
youichiro <cinnamon416@gmail.com>
2021-11-27 09:29:40 +0900

    ChakraProvider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,14 @@
 import { ApolloProvider } from '@apollo/client'
 import type { AppProps } from 'next/app'
 import apolloClient from '../lib/apollo'
+import { ChakraProvider } from '@chakra-ui/react'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ApolloProvider client={apolloClient}>
-      <Component {...pageProps} />
+      <ChakraProvider>
+        <Component {...pageProps} />
+      </ChakraProvider>
     </ApolloProvider>
   )
 }
