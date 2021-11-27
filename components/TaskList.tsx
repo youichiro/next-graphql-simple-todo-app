f1cb780d6dd52f0fb632e56cf37807aea1b68999
youichiro <cinnamon416@gmail.com>
2021-11-27 11:43:23 +0900

    完了機能

--- components/TaskList.tsx
+++ components/TaskList.tsx
@@ -1,4 +1,4 @@
-import { gql, useQuery } from '@apollo/client'
+import { gql, useMutation, useQuery } from '@apollo/client'
 import { Task } from '.prisma/client'
 import { Checkbox, List, ListItem, Flex } from '@chakra-ui/react'
 import TaskDeleteButton from './TaskDeleteButton'
@@ -13,21 +13,47 @@ export const AllTasksQuery = gql`
   }
 `
 
+const UpdateTaskMutation = gql`
+  mutation UpdateTask($id: Int!, $title: String!, $done: Boolean!) {
+    updateTask(id: $id, title: $title, done: $done) {
+      id
+    }
+  }
+`
+
 const TaskList: React.FC = () => {
   const { data, loading, error } = useQuery(AllTasksQuery)
+  const [updateTask, mutation] = useMutation(UpdateTaskMutation, {
+    refetchQueries: [AllTasksQuery],
+  })
+
+  const handleCheckboxClick = (task: Task) => {
+    updateTask({
+      variables: {
+        id: task.id,
+        title: task.title,
+        done: !task.done,
+      },
+    })
+  }
 
   if (loading) return <p>Loading...</p>
   if (error) return <p>Error: {error.message}</p>
+  if (mutation.error) return <p>Error: {mutation.error.message}</p>
 
   return (
     <List>
       {data.tasks.map((task: Task) => (
         <ListItem key={task.id}>
           <Flex justify='space-between'>
-          <Checkbox colorScheme='teal' isChecked={task.done}>
-            {task.title}
-          </Checkbox>
-          <TaskDeleteButton taskId={task.id} />
+            <Checkbox
+              colorScheme='teal'
+              isChecked={task.done}
+              onChange={() => handleCheckboxClick(task)}
+            >
+              {task.title}
+            </Checkbox>
+            <TaskDeleteButton taskId={task.id} />
           </Flex>
         </ListItem>
       ))}
